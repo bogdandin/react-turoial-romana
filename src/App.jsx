daa5095d4a8a7fb1d738d36f3d8556108db71bbd
Din Bogdan <bogdan862001@gmail.com>
2020-03-31 10:42:53 +0300

    Firt Component 1 Done https://www.youtube.com/watch?v=uf524ffDHM8

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Firts Componenent</h1>
+                <p> This is my first component in React</p>
+
+            </div>
+        )
+
+    }
+
+}
+
+export default App
